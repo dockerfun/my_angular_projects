ac53eab05d514b98b0a9379b4cb5bf7f68daed5c
dockerfun <hmiklosazure@gmail.com>
2023-05-30 14:48:01 +0200

    Components and data binding practice

---
+++ components_and_data_binding/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,47 @@
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css'],
+})
+export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>();
+
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
+
+  // newServerName = '';
+  // newServerContent = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onAddServer(nameInput: HTMLInputElement) {
+    this.serverCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value,
+    });
+  }
+
+  onAddBlueprint(contentInput: HTMLInputElement) {
+    this.blueprintCreated.emit({
+      serverName: contentInput.value,
+      serverContent: this.serverContentInput.nativeElement.value,
+    });
+  }
+}
